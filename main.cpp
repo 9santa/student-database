537ddec24e89d88e2fb03550699a76a3b82c53a4
9santa <wotegorb@mail.ru>
2025-05-24 15:11:48 +0300

    make interactive console db program

---
+++ main.cpp
@@ -0,0 +1,408 @@
+#include <bits/stdc++.h>
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <chrono>
+#include <format>
+#include <limits>
+
+
+using namespace std;
+
+using ll = long long;
+
+#define F first
+#define S second
+#define pb push_back
+#define all(x) x.begin(), x.end()
+#define debug(x) cout << __FUNCTION__ << ":" << __LINE__ << " " << #x << " = " << x << endl
+
+const int MOD = 998244353;
+const double PI = 3.141592653589793;
+bool flag = 0;
+
+class StudentGroup {
+    private:
+        string name;        // Наименование *
+        int year;           // Год набора **
+        char degree;        // B (Bаклавриат) / С (Специалитет) / М (Магистратура)
+        string faculty;     // Факультет
+        string captain;     // Староста
+        int studentCount;   // Кол-во человек
+
+    public:
+        // CONSTRUCTOR
+        StudentGroup(const string &name, const int &year, const char &degree, const string &faculty,
+            const string &captain, const int &studentCount) : name(name), year(year), degree(degree),
+            faculty(faculty), captain(captain), studentCount(studentCount) {}
+
+        // GETTERS
+        string getName() const { return name; }
+        int getYear() const { return year; }
+        char getDegree() const { return degree; }
+        string getFaculty() const { return faculty; }
+        string getCaptain() const { return captain; }
+        int getStudentCount() const { return studentCount; }
+
+        // GETTERS
+        void setName(string n) { name = n; }
+        void setYear(int y) { year = y; }
+        void setDegree(char deg) { degree = deg; }
+        void setFaculty(string fac) { faculty = fac; }
+        void setCaptain(string cap) { captain = cap; }
+        void setStudentCount(int count) { studentCount = count; }
+
+        // вывод информации о группе (записи)
+        void print() const {
+            string displayFaculty = faculty.substr(0,39);
+            if(faculty.length() > 25) displayFaculty += "...";
+            cout << left
+                 << "| " << setw(14) << name
+                 << " | " << setw(10) << year
+                 << " | " << setw(40) << faculty
+                 << " | " << setw(50) << captain
+                 << " | " << setw(8) << studentCount << '\n';
+        }
+
+        void saveToFile(ofstream &output) const {
+            output << name << ';' << year << ';' << degree << ';' << faculty << ';'
+                << captain << ';' << studentCount << '\n';
+        }
+
+        static StudentGroup loadFromFile(ifstream &input){
+            string line;
+            getline(input, line);
+
+            stringstream ss(line);
+            vector<string> cells;
+            string cell;
+
+            while(getline(ss, cell, ',')){
+                cells.push_back(cell);
+            }
+
+            if(cells.size() != 6){
+                throw runtime_error("Некорректный формат строки в файле!\n");
+            }
+
+            return StudentGroup(
+                    cells[0],           // name
+                    stoi(cells[1]),     // year
+                    cells[2][0],        // degree (first character)
+                    cells[3],           // faculty
+                    cells[4],           // captain
+                    stoi(cells[5]));    // studentCount
+
+        }
+};
+
+class Database {
+    private:
+        vector<StudentGroup> groups;
+
+    public:
+
+        // GETTERS
+        int getGroupCount() const { return groups.size(); }
+
+        // добавление группы
+        void addGroup() {
+            string name, faculty, captain;
+            int year, count;
+            char degree;
+
+            cout << "Введите данные группы для добавления:" << endl;
+
+            cout << "Название группы: ";
+            cin.ignore();
+            getline(cin, name);
+
+            cout << "Год набора: "; cin >> year;
+
+            cout << "Степень обучения (char = B/C/M): "; cin >> degree;
+
+            cout << "Факультет: ";
+            cin.ignore(); // пропускаем символ новой строки в буфере
+            getline(cin, faculty);
+            
+            cout << "Староста: "; getline(cin, captain);
+            cout << "Количество студентов: "; cin >> count;
+
+            groups.emplace_back(name, year, degree, faculty, captain, count);
+            cout << "\nГруппа успешно добавлена!\n";
+            
+        }
+        
+        // удаление группы (по номеру)
+        void deleteGroup(int index) {
+            // проверка правильности индекса
+            if(index < 0 || index >= groups.size()) {
+                cerr << "Введен неверный индекс!\n";
+                return;
+            }
+            
+            groups.erase(groups.begin() + index);
+            cout << "Группа успешно удалена.\n";
+        }
+
+        void view() const {
+            if(groups.empty()){
+                cout << "База данных абсолютно пуста.\n";
+                return;
+            }
+
+            cout << left
+                 << setw(14) << "\nНазвание"
+                 << " | " << setw(10) << "Год"
+                 << " | " << setw(50) << "Факультет"
+                 << " | " << setw(40) << "Староста"
+                 << " | " << setw(8) << "Кол-во\n";
+            cout << string(112, '-') << '\n'; // разделитель
+
+            for(const auto &group : groups){
+                group.print();
+            }
+        }
+
+
+
+        void searchByName(const string &name){
+            bool found = false;
+            for(const auto &group : groups){
+                if(group.getName() == name){
+                    found = true;
+                    cout << "Группа найдена!\n";
+                    group.print();
+                }
+            }
+            if(!found) {
+                cout << "Группы с таким названием не существует в базе.\n";
+            }
+        }
+        
+        // Сортировка по названию
+        void sortByName() {
+            sort(groups.begin(), groups.end(), [](const StudentGroup &a, const StudentGroup &b){
+                    return a.getName() < b.getName();
+                    });
+            cout << "Сортировка по названию успешна." << endl;
+        }
+
+        // Загрузка из файла
+        void loadFromFile(const string &filename){
+            ifstream File(filename);
+            if(!File){
+                cerr << "Ошибка открытия файла для чтения!" << endl;
+                return;
+            }
+
+            // Очищаем все предыдущие записи
+            groups.clear();
+
+            while(File.peek() != EOF) {
+                groups.push_back(StudentGroup::loadFromFile(File));
+            }
+            File.close();
+            cout << "База данных успешно загружена. Записей: " << groups.size() << endl;
+        }
+
+        void saveToFile(const string &filename) const{
+            ofstream File(filename);
+            if(!File){
+                cerr << "Ошибка открытия файла для записи!" << endl;
+                return;
+            }
+
+            for(const auto &group : groups){
+                group.saveToFile(File);
+            }
+
+            File.close();
+            cout << "База данных сохранена. Записей: " << groups.size() << endl;
+        }
+
+        // Фильр по году (диапазон)
+        void filterYear(int start, int end){
+            auto filtered = count_if(groups.begin(), groups.end(),
+                    [start, end](const StudentGroup &g){
+                    return g.getYear() >= start && g.getYear() <= end;
+                    });
+
+            if(filtered == 0){
+                cout << "Группы в диапазоне " << start << "-" << end << " не найдены.\n";
+                return;
+            }
+            
+            cout << "Группы в диапазоне " << start << "-" << end << ":\n";
+            for_each(groups.begin(), groups.end(),
+                    [start, end](const StudentGroup &g){
+                    if(g.getYear() >= start && g.getYear() <= end){
+                        g.print();
+                    }
+                });
+        }
+
+        void printFacultyByCount(const string &faculty){
+            vector<StudentGroup> filtered;
+            for(const auto &g : groups){
+                if(g.getFaculty() == faculty) filtered.push_back(g);
+            }
+            sort(filtered.begin(), filtered.end(), [](const StudentGroup &a, const StudentGroup &b){
+                    return a.getStudentCount() < b.getStudentCount();
+                    });
+            if(filtered.empty()){
+               cout << "Группа факультета " << faculty << " не найдены." << endl;
+               return;
+            }
+            cout << "Групаы факультета " << faculty << " (по численности):";
+            for(const auto &g : filtered){
+                g.print();
+            }
+        }
+
+        void createSeperateDBs(Database &bachelorDB, Database &specialistDB, Database &masterDB){
+            for(const auto &g : groups){
+                switch(g.getDegree()){
+                    case 'B':
+                        bachelorDB.groups.push_back(g);
+                        break;
+                    case 'C':
+                        specialistDB.groups.push_back(g);
+                        break;
+                    case 'M':
+                        masterDB.groups.push_back(g);
+                        break;
+                }
+            }
+            // Очищаем старую DB
+            groups.clear();
+            cout << "База данных разделена по типам обучения." << endl;
+        }
+};
+
+// print menu to the console
+void printMenu() {
+    cout << "\n1. Загрузить БД из файла\n";
+    cout << "2. Просмотр БД\n";
+    cout << "3. Добавить группу\n";
+    cout << "4. Удалить группу\n";
+    cout << "5. Сохранить БД в файл\n";
+    cout << "6. Сортировать БД по названию\n";
+    cout << "7. Поиск группы по названию\n";
+    cout << "8. Вывести группы факультета по численности\n";
+    cout << "9. Создать отдельные БД по типам обучения\n";
+    cout << "0. Выход\n";
+}
+
+int main()
+{
+    
+    /*
+    ifstream inFile("groups.csv");
+    ofstream output("output.txt");
+    if(!input.is_open() || !output.is_open()){
+        cerr << "Ошибка открытия файла\n";
+        return 1;
+    }
+    */
+    
+    //ios::sync_with_stdio(0);
+    //cin.tie(0);
+    //cout.tie(0);
+
+    clock_t z = clock();
+
+    Database db;
+    Database bachelorDB, specialistDB, masterDB;
+    string filename;
+    int choice;
+
+    do{
+        printMenu();
+        cin >> choice;
+        cin.ignore();
+
+        string searchName, faculty;
+
+        switch(choice){
+            case 1:
+                cout << "Введите имя файла: ";
+                getline(cin, filename);
+                db.loadFromFile(filename);
+                break;
+
+            case 2:
+                db.view();
+                break;
+
+            case 3:
+                db.addGroup();
+                break;
+
+            case 4:
+                int index;
+                cout << "Введите номер группы для удаления: ";
+                cin >> index;
+                db.deleteGroup(index);
+                break;
+
+            case 5:
+                cout << "Введите имя файла: ";
+                getline(cin, filename);
+                db.saveToFile(filename);
+                break;
+
+            case 6:
+                db.sortByName();
+                break;
+
+            case 7:
+                cout << "Введите название группы для поиска: ";
+                getline(cin, searchName);
+                db.searchByName(searchName);
+                break;
+
+            case 8:
+                int start, end;
+                cout << "Введите начальный год: ";
+                cin >> start;
+                cout << "Введите конечный год: ";
+                cin >> end;
+                db.filterYear(start, end);
+                break;
+
+            case 9:
+                cout << "Введите название факультета: ";
+                getline(cin, faculty);
+                db.printFacultyByCount(faculty);
+                break;
+
+            case 10:
+                db.createSeperateDBs(bachelorDB, specialistDB, masterDB);
+                cout << "Бакалавриват (" << bachelorDB.getGroupCount() << " групп)\n";
+                cout << "Специалитет (" << specialistDB.getGroupCount() << " групп)\n";
+                cout << "Магистратура (" << masterDB.getGroupCount() << " групп)\n";
+                break;
+
+            case 0:
+                cout << "Выход из программы.\n";
+                break;
+
+            default:
+                cout << "Неверный выбор. Попробуйте снова.\n";
+        }
+    } while(choice != 0);
+
+    
+    //printMenu();
+    //cohle(input, output);
+
+    //input.close();
+    //output.close();
+
+    cerr << "\nRun Time : " << ((double)(clock() - z) / CLOCKS_PER_SEC) << '\n';
+    
+
+    return 0;
+}
+
